130981995ea0943dcef55b35367f5f8e29664e58
zhaoyuuu <13387281442@163.com>
2023-04-18 16:04:58 +0800

    feat: enrich interview/react

--- demo.js
+++ demo.js
@@ -1,22 +1,15 @@
-Function.prototype._apply = function (context = window, args) {
-  // 传参形式变了，别的不变
-  const key = Symbol('key')
-  context[key] = this
-  const result = context[key](...args)
-  delete context[key]
-  return result
+// const obj = {
+//   a: xyz,
+//   b: uvm,
+// }
+// 111{obj.a}555{obj.b}666{obj.g}
+// =>111xyz555uvm666{obj.g}
+function fn(str) {
+  let res
+  res = str.replace('{obj.a}', 'xyz')
+  res = res.replace('{obj.b}', 'uvm')
+  return res
 }
-
-function sayFn(a, b) {
-  console.log(`${a} + ${b} = ${a + b}`)
-  console.log(`我是${this.name}, 我今年${this.age}岁`)
-}
-const me = {
-  name: 'zhaoyuuu',
-  age: 20,
-}
-sayFn._apply(me, [1, 2])
-//1 + 2 = 3
-//我是zhaoyuuu, 我今年20岁
-console.log(me)
-//{ name: 'zhaoyuuu', age: 20 } （不影响原对象）
+const str = '111{obj.a}555{obj.b}666{obj.g}'
+const res = fn(str)
+console.log(res)
---
+++ docs/interview/css.md
@@ -0,0 +1,28 @@
+## 重绘 vs 重排
+
+🟡 以下操作引起**重绘**：
+
+- color
+- border-style
+- border-radius
+- text-decoration
+- box-shadow
+- background
+- ...
+
+🔴 以下操作引起**重排**：
+
+- 页面初次渲染
+- 添加/删除可见的 DOM 元素
+- 改变元素位置
+- 改变元素尺寸
+- 改变浏览器窗口大小 resize
+- 查询 DOM 属性，比如 offsetWidth offsetHeight
+
+💡 **优化建议：减少重排次数、减小重排范围**
+
+1. **样式集中改变** => 改变 class 来一次性改变样式
+2. 将 **DOM 离线**
+   1. 需要频繁修改样式时，display:none 隐藏，修改完之后再显示
+   2. absolute / fixed 脱离文档流
+   3. 善用内存。在内存中多次操作 DOM，再添加到整个 DOM 树。
